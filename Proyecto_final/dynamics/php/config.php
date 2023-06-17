6c90ba659ed8d8526eebbffec8c56d4c3f78bd27
Daniel Rodríguez <87953599+danibutnotdani@users.noreply.github.com>
2023-06-17 11:25:11 -0600

    Pruebas config.php

--- Proyecto_final/dynamics/php/config.php
+++ Proyecto_final/dynamics/php/config.php
@@ -1,11 +1,10 @@
 <?php
     const DBHOST = "localhost";
     const DBUSER = "root";
-    const PASSWORD = "karen"; //QUITAR PASSWORD
+    const PASSWORD = 'karen'; //QUITAR PASSWORD
     const DB = "proyectoFinal"; 
     function connect ()
     {
     $conexion = mysqli_connect (DBHOST, DBUSER, PASSWORD, DB);
     return $conexion;
     }
-?> 
