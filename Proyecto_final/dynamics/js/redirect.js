e33fb0bd0c105e0a12b4d7937b987ba29ddf041c
Morales Olivares <321099604@alumno.enp.unam.mx>
2023-06-21 15:09:13 -0600

    feat(redirect.js): añado funcion de redirigir desde landing page a seleccionRol.php y a inicioDeSesion.html

--- Proyecto_final/dynamics/js/redirect.js
+++ Proyecto_final/dynamics/js/redirect.js
@@ -1,9 +1,9 @@
 function redirect() {
 
-    window.location.href = "./php/seleccionRol.php";
+    window.location.href = "./dynamics/php/seleccionRol.php";
 }
 function redirecttwo() {
 
-    window.location.href = "inicioSesion.php";
+    window.location.href = "./dynamics/php/inicioDeSesion.html";
 }
 // solo es para que te redirija al click a registrarte o a iniciar sesion
