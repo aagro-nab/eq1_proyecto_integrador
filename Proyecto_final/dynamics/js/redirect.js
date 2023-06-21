230cc18aba5647b991d3598864320e5abecbbfbc
Morales Olivares <321099604@alumno.enp.unam.mx>
2023-06-21 14:07:27 -0600

    feat: añade archivo para redirigir desde landing page a logIn y seleccion de rol, redirect.js

---
+++ Proyecto_final/dynamics/js/redirect.js
@@ -0,0 +1,9 @@
+function redirect() {
+
+    window.location.href = "./php/seleccionRol.php";
+}
+function redirecttwo() {
+
+    window.location.href = "inicioSesion.php";
+}
+// solo es para que te redirija al click a registrarte o a iniciar sesion
