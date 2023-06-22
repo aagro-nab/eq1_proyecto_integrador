a324d7b306d213e7278f6398220c7b66715f720f
Chu <jesusatorizf@gmail.com>
2023-06-21 20:19:19 -0600

    FEAT(php): añade cookies

--- Proyecto_final/dynamics/php/inicioSesion.php
+++ Proyecto_final/dynamics/php/inicioSesion.php
@@ -3,11 +3,60 @@
 $include = include("./config.php");
 $con = connect();
 session_start();
+$cuki = (isset($_COOKIE['usuario']) && $_COOKIE['usuario'] != "")? $_COOKIE['usuario'] : FALSE;
+$cuki_rol = (isset($_COOKIE['rol']) && $_COOKIE['rol'] != "")? $_COOKIE['rol'] : FALSE;
+if($cuki && $cuki_rol){
+    $_SESSION['rol'] = $_COOKIE['rol'];
+    header("location:./principal.php");
+}
+
+echo '
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicia sesión</title>
+  <link rel="stylesheet" href="../../statics/styles/inicioSesion.css">
+  <link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Escudo_enp_6.svg/927px-Escudo_enp_6.svg.png" type="image/x-icon">
+</head>
+<body>
+<main class="Registros">
+  <article class="iniciosesion">
+    <form method="post" action="./inicioSesion.php" target="_self">
+      <p class="titulo-s" id="titulo-iniciosesion" tabindex="1">Iniciar sesión</p>
+
+      <p class="texto-m">Nombre de usuario / Email :</p>
+      <input tabindex="2" type="text" class="input-datos" id="login-username" name="login-username">
+
+      <p class="texto-m" for="login-password">Contraseña:</p>
+      <input tabindex="3" type="password" class="input-datos" id="login-password" name="login-password">
+
+      <div class="row">
+        <input type="checkbox" id="remember-me" name="remember-me" tabindex="4">
+        <label for="remember-me" class="texto-m">Recordar usuario</label>
+      </div><br>
+
+      <div class="row-center">
+        <button type="submit" class="boton-submit" value="Iniciar sesión" tabindex="5">¡Vamos!</button>
+      </div>
+
+      <div class="row-center">
+        <a href="seleccionRol.php" class="boton-submit register-link" tabindex="6">Regístrate</a>
+      </div>
+
+      <a href="#" tabindex="6">¿Olvidaste tu contraseña?</a>
+    </form>
+  </article>
+</main>
+</body>
+</html>
+';
 
 //recibe los datos de inicioDeSesion
 $username = (isset($_POST["login-username"]) && $_POST["login-username"] != "")? $_POST["login-username"] : NULL;
 $pswrd = (isset($_POST["login-password"]) && $_POST["login-password"] != "")? $_POST["login-password"] : NULL;
-$remember = (isset($_POST["remember-me"]) && $_POST["remember-me"] != "")? $_POST["remember-me"] : NULL;
+$remember = (isset($_POST["remember-me"]) && $_POST["remember-me"] != "")? $_POST["remember-me"] : FALSE;
 
 //consultar datos
 if($include && $con){
@@ -22,27 +71,19 @@ if($include && $con){
     $datos = mysqli_fetch_assoc($query); //recibe los datos de $query para poder procesarlos
     $rol = mysqli_fetch_array($query_rol);
 
-    $r = $rol[0];//
-
     if($datos){//si $datos es TRUE redireccionar a la página principal 
+        $r = $rol[0];//
         $_SESSION['username'] = $username;// "$_SESSION es una variable que va a guardar el username para poder iniciar sesion en la página principal 
+        $_SESSION['rol'] = $r;
 
-        //revisa el valor de $rol, y cuando encuentre una coincidencia es la direccion que va a tomar
-        switch($r){
-            case "estudiante":
-                header("location:./paginaPrincipal.php");
-                break;
-            case "moderador":
-                header("location:[pagina para moderadores]");//cambiar a la pagina correcta
-                break;
-            case "administrador":
-                header("location:[pagina para administradores]");//cambiar a la pagina correcta
-                break;
+        if($remember){
+            setcookie("rol", $_SESSION['rol'], time() + 60 * 60 * 24);//dura un día
+            setcookie("usuario", $_SESSION['username'], time() + 60 * 60 * 24);//dura un día
         }
-    }else{
-        //si es FALSE mandar un mensaje de error y redireccionar otra vez al inicio de sesion
-        header("location:./inicioDeSesion.html");
-        echo "Usuario y/o contraseña incorrectos";//revisar donde se va a imprimir esto
+        header("location:./principal.php");
     }
+    // }else{
+    //     //si es FALSE mandar un mensaje de error y redireccionar otra vez al inicio de sesion
+    // }
 }
 ?>
--- Proyecto_final/dynamics/php/principal.php
+++ Proyecto_final/dynamics/php/principal.php
@@ -1,4 +1,22 @@
-<!DOCTYPE html>
+<?php
+include ("./config.php");
+$con = connect();
+session_start();
+
+$r = $_SESSION['rol'];
+//revisa el valor de $rol, y cuando encuentre una coincidencia es la direccion que va a tomar para el script
+switch($r){
+    case "estudiante":
+        $rol = "principalAlumno.js";
+        break;
+    case "moderador":
+        $rol = "principalModerador.js";
+        break;
+    case "administrador":
+        $rol = "principalAdmin.js";
+        break;
+}
+echo '<!DOCTYPE html>
 <html>
 <head>
     <title>Página Principal</title>
@@ -37,11 +55,12 @@
         <a href="#" id="crearNuevoButton6" class="btn2" style="display: none;"><i class=""></i><span class="button-text"></span></a>
     </div>
 </main>
-<script src="../js/principalAdmin.js"></script>
+<script src="../js/'.$rol.'"></script>
 <script src="../js/modalesGenerales.js"></script>
 <script src="../js/modalesForos.js"></script>
 <script src="../js/modalesPreguntas.js"></script>
 <script src="../js/modalesExtravios.js"></script>
 <script src="../js/modalesMarket.js"></script>
 </body>
-</html>
+</html>'
+?>
