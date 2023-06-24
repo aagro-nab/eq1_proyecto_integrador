de7484fd4a7bb3bfabcb21c60501a35aaa440d1e
Morales Olivares <321099604@alumno.enp.unam.mx>
2023-06-23 22:39:44 -0600

    style(config.php): añade identación

--- Proyecto_final/dynamics/php/config.php
+++ Proyecto_final/dynamics/php/config.php
@@ -1,17 +1,18 @@
-<?php 
-const HOST="localhost";
-const USER="equipo01";
-const PASS="equser01all";
-const DB="proyectoFinal";
+<?php
+const HOST = "localhost";
+const USER = "equipo01";
+const PASS = "equser01all";
+const DB = "proyectoFinal";
 
 const nRespaldo = "proyectoFinalRespaldo";
 
-function connect(){
-    $conexion = mysqli_connect(HOST,USER,PASS,DB);
+function connect()
+{
+    $conexion = mysqli_connect(HOST, USER, PASS, DB);
 
-    if(!$conexion){
+    if (!$conexion) {
         echo "Error: No se pudo conectar a MySQL. <br> ";
-        echo "Error: " . mysqli_connect_errno() ."<br>";
+        echo "Error: " . mysqli_connect_errno() . "<br>";
         echo "Error: " . mysqli_connect_error() . "<br>";
         exit;
     }
@@ -21,7 +22,7 @@ function connect(){
 //Si usan MariaDB cambien los datos de HOST, USER, PASS y DB y tambien el picoparentesis por -r
 function generarRespaldo()
 {
-    $command = "C:\xampp\mysql\bin\mysqldump.exe -u" . USER . " -p" . PASS . " " . DB . " > " .nRespaldo;
+    $command = "C:\xampp\mysql\bin\mysqldump.exe -u" . USER . " -p" . PASS . " " . DB . " > " . nRespaldo;
 
     system($command, $output);
 
